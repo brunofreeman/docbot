6d5c5c9537061eeddd55b95fd40c5f3485b3082a
brunofreeman <brunofreeman21@gmail.com>
2022-04-06 02:35:14 -0700

    Add script copying local repo to HPC

---
+++ push.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ "${PWD##*/}" != "docbot" ]; then
+  echo "usage: $0 must be run in docbot/, not ${PWD##*/}/"
+  exit 1
+fi
+
+if [ "$#" -ne 1 ]; then
+  echo "usage: $0 <username>" >&2
+  exit 1
+fi
+
+read -r -p "You are about to OVERWRITE your HPC docbot with your local version. Continue? (y/n) " yn
+
+case $yn in
+	[yY] ) scp -r . "$1"@login.hpc.caltech.edu:/home/"$1"/docbot;;
+	[nN] ) echo "Overwrite aborted.";;
+	* ) echo "Invalid response. Overwrite aborted.";;
+esac
