9ee44b21beaf724d225642b6e49ec32a67aedc11
Mihir M. Borkar <mborkar@login2.cm.cluster>
2022-04-26 17:37:27 -0700

    fixed dataset_chexnet. model is now training

--- src/dataset_chexnet.py
+++ src/dataset_chexnet.py
@@ -86,12 +86,13 @@ class CheXpertTrainingDataset(Dataset):
 
         if not os.path.exists(img_path):
             print(f"Dataset failed: could not find {img_path} for data point {idx}. Using all zeroes as fallback.")
-            img: np.ndarray = np.zeros(shape=(3, *RESOLUTION), dtype=np.float32)
-            img_tensor: torch.Tensor = torch.from_numpy(img)
+            img: np.ndarray = np.zeros(shape=(*RESOLUTION, 3), dtype=np.float32)
+
         else:
             # convert the image into a PyTorch tensor and normalize [0, 255] -> [0, 1]
             img: np.ndarray = np.array(cv2.imread(img_path, COLOR_MODE),dtype=np.float32)
             img /= MAX_PIXEL_INTENSITY
+
         img_tensor: torch.Tensor = torch.from_numpy(img)
         img_tensor = img_tensor.permute(2, 1, 0) # put the channels dimension first
 
