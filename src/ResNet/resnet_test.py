15fd2064b469f1f8381d5ca353f98f911acf9cfa
Michael D. Batchev <mbatchev@login1.cm.cluster>
2022-05-03 14:20:39 -0700

    Started ResNet implementation

---
+++ src/ResNet/resnet_test.py
@@ -0,0 +1,91 @@
+import sys
+from typing import List, Tuple
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+from torch.utils.data import Dataset, DataLoader
+from torchsummary import summary
+from dataset_resnet import CheXpertTrainingDataset
+
+BATCH_SIZE: int = 16
+N_EPOCHS: int = 128
+
+IN_DIM: Tuple[int, int, int] = (3, 512, 512)
+OUT_DIM: int = 14
+
+
+def main(argv: List[str]) -> None:
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(f"device: {device}")
+
+    data_loader = DataLoader(
+        CheXpertTrainingDataset(device), batch_size=BATCH_SIZE, shuffle=True
+    )
+
+
+    # model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet152', pretrained=True)
+    model  = torchvision.models.resnet152(pretrained=False)
+
+    # had to switch model.classifier to model.fc to access last layer, also 2048 to interface
+    model.fc = nn.Sequential(
+            nn.Linear(2048, 512),
+            nn.ReLU(),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Linear(256, OUT_DIM),
+            nn.Tanh()
+    )
+    model = model.to(device)
+    print(model)
+    criterion = nn.MSELoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+    for epoch_idx in range(N_EPOCHS):
+
+        f = open("./out/resnet_test_live.txt", "a")
+        title: str = f"Epoch {epoch_idx+1:03d}/{N_EPOCHS}:"
+        print(f"{title}\n{'-' * len(title)}")
+        f.write(f"{title}\n{'-' * len(title)}\n")
+        f.close()
+        training_loss: float = 0.0
+        model.train()
+
+        for batch_idx, (images, labels) in enumerate(data_loader):
+            #print(images)
+            # erase acculmulated gradients
+            optimizer.zero_grad()
+
+            # forward pass
+
+            output = model(images)
+            # print("images shape", images.shape)
+            # print("output shape", output.shape)
+            # print("labels shape", labels.shape)
+            # compute loss
+            loss = criterion(output, labels)
+
+            # backward pass
+            loss.backward()
+
+            # update weights
+            optimizer.step()
+
+            training_loss += loss.item()
+
+        # update loss and training accuracy
+        training_loss /= len(data_loader)
+
+        save_path: str = f"./out/resnet_v1_{(epoch_idx + 1):03d}.pt"
+        f = open("./out/resnet_test_live.txt", "a")
+        f.write(f"loss: {training_loss:0.4f}\n")
+        f.close()
+        print(f"loss: {training_loss:0.4f}")
+        print(f"Saving model to {save_path}")
+        torch.save(model.state_dict(), save_path)   
+    f.close()    
+     
+
+if __name__ == "__main__":
+    main(sys.argv)
---
+++ src/ResNet/resnet_test_predict.py
@@ -0,0 +1,101 @@
+#TODO: This file is not ready yet
+
+import sys
+from typing import Dict, List, Tuple
+import numpy as np
+import torch
+import torchvision
+import torch.nn as nn
+import cv2
+import pandas as pd
+from torchvision import transforms
+
+MODEL_PATH: str = "./out/resnet_v1_001.pt"
+PREDICITON_CSV_PATH: str = "./out/resnet_v1_001.csv"
+
+OUT_DIM: int = 14
+
+MAX_PIXEL_INTENSITY: int = 255
+COLOR_MODE: int = cv2.IMREAD_COLOR
+RESOLUTION: Tuple[int, int] = (512, 512)
+INTERPOLATION: int = cv2.INTER_CUBIC
+
+DATASET_NAME: str = (
+    ("greyscale" if COLOR_MODE == cv2.IMREAD_GRAYSCALE else "color") +
+    f"{RESOLUTION[0]}x{RESOLUTION[1]}"
+)
+
+DATASET_PATH: str = f"/groups/CS156b/2022/team_dirs/mborkar_docbot/{DATASET_NAME}"
+TEST_ID_PATH: str = "/groups/CS156b/data/student_labels/test_ids.csv"
+
+ID_COL: str = "Id"
+
+PATHOLOGIES: List[str] = [
+    "No Finding",
+    "Enlarged Cardiomediastinum",
+    "Cardiomegaly",
+    "Lung Opacity",
+    "Lung Lesion",
+    "Edema",
+    "Consolidation",
+    "Pneumonia",
+    "Atelectasis",
+    "Pneumothorax",
+    "Pleural Effusion",
+    "Pleural Other",
+    "Fracture",
+    "Support Devices"
+]
+
+
+def main(argv: List[str]) -> None:
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(f"device: {device}")
+    
+    # load pre-trained model
+    model = torchvision.models.densenet121(pretrained=True) 
+    model.classifier = nn.Sequential(
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Linear(256, OUT_DIM),
+            nn.Tanh()
+    )
+    model = model.to(device)
+    model.load_state_dict(torch.load(MODEL_PATH, map_location=device))
+    model.eval()
+
+    # use model to populate predictions for a given row
+    def predict(id: str, path: str) -> pd.Series:
+        img_path: str = f"{DATASET_PATH}/{path}"
+        img: np.ndarray = np.array([cv2.imread(img_path, COLOR_MODE)], dtype=np.float32)
+        img /= MAX_PIXEL_INTENSITY
+        img_tensor: torch.Tensor = torch.from_numpy(img).to(device)
+        img_tensor = img_tensor.permute(0, 3, 1, 2)  # put the channels dimension first
+        preprocess = transforms.Compose([
+                                     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+                                     ])
+        img_tensor = preprocess(img_tensor)                    
+        prediction = model(img_tensor)[0].cpu().detach().numpy()
+        prediction = prediction.tolist()
+        prediction.insert(0, id)
+        return prediction
+        
+    
+    # get a data frame with (Id, Path) pairs
+    to_predict: pd.DataFrame = pd.read_csv(TEST_ID_PATH)
+
+    # empty prediciton data frame
+    predictions: pd.DataFrame = pd.DataFrame(columns=[ID_COL, *PATHOLOGIES])
+
+    # append a row with a prediction for each (Id, Path) test pair
+    for i, row in to_predict.iterrows():
+        predictions.loc[i] = predict(str(row[ID_COL]), row["Path"])
+
+    # write to CSV
+    predictions.to_csv(PREDICITON_CSV_PATH, index=False)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
